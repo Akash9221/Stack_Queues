50967512899949f3f22520fce3da106603a8d263
“akashmane3938” <maneakash3938@gmail.com>
2022-12-06 23:37:52 -0800

    Stack And Queues

---
+++ Stack_Queues_Solutions/Stack_Queues_Solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack_Queues_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack and Queues");
+        }
+    }
+}
