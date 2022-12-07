99c7b9c5fe7f7b4196849b0401d32254fc53402d
“akashmane3938” <maneakash3938@gmail.com>
2022-12-07 00:19:07 -0800

    Peek And Pop From Stack

--- Stack_Queues_Solutions/Stack_Queues_Solutions/Program.cs
+++ Stack_Queues_Solutions/Stack_Queues_Solutions/Program.cs
@@ -6,11 +6,24 @@ namespace Stack_Queues_Solutions
     {
         static void Main(string[] args)
         {
+            //Stack1
+
+            //Stack s = new Stack();//Create Stack Object
+            //s.Add(70);
+            //s.Add(30);
+            //s.Add(56);
+            //s.Display();//Call Display Method
+
+
+            //Stack2
             Stack s = new Stack();//Create Stack Object
             s.Add(70);
             s.Add(30);
             s.Add(56);
-            s.Display();//Call Display Method
+            s.Display();//Call Display Metho
+            s.peek();//Call Peek Method
+            s.pop();//Call pop Method
+            s.Display();
         }
     }
 }
