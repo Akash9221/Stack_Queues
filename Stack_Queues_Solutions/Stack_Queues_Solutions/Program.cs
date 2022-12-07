1eb9449f3ff7b7689c8fcc5f6c06fb9428ef4286
“akashmane3938” <maneakash3938@gmail.com>
2022-12-07 00:28:30 -0800

    Add Data Into Queue

--- Stack_Queues_Solutions/Stack_Queues_Solutions/Program.cs
+++ Stack_Queues_Solutions/Stack_Queues_Solutions/Program.cs
@@ -16,14 +16,21 @@ namespace Stack_Queues_Solutions
 
 
             //Stack2
-            Stack s = new Stack();//Create Stack Object
-            s.Add(70);
-            s.Add(30);
-            s.Add(56);
-            s.Display();//Call Display Metho
-            s.peek();//Call Peek Method
-            s.pop();//Call pop Method
-            s.Display();
+            //Stack s = new Stack();//Create Stack Object
+            //s.Add(70);
+            //s.Add(30);
+            //s.Add(56);
+            //s.Display();//Call Display Metho
+            //s.peek();//Call Peek Method
+            //s.pop();//Call pop Method
+            //s.Display();
+
+            //Queue1
+            Queue q = new Queue();//Create object Queue
+            q.Enqueue(70);//Add Datat Into The Queue
+            q.Enqueue(30);
+            q.Enqueue(56);
+            q.Display();//Call Display Method
         }
     }
 }
